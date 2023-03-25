5ca2e5082ee3e79e1b4669e5f7b072a4aee43d54
jjhome123 <76233999+jjhome123@users.noreply.github.com>
2023-03-25 19:26:53 +0900

    Add files via upload

--- auctions/admin.py
+++ auctions/admin.py
@@ -5,5 +5,4 @@ from .models import User, Listing, Bid, Watchlist, Comment
 admin.site.register(User)
 admin.site.register(Listing)
 admin.site.register(Bid)
-admin.site.register(Watchlist)
 admin.site.register(Comment)
